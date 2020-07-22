4cf48f1c67c3ec11da993925605bdb4835cc5ac2
Raúl <osvaldoracm@gmail.com>
2020-07-22 17:55:32 -0500

    inital configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import SpendingForm from './components/SpendingForm'
 
 const App = () => {
   return (
-    <div className="container mt-3">
-      <h1>Hola mundo</h1>
+    <div className="container mt-5 text-center">
+      <h1>Weekly expense</h1>
+
+      <div className="row">
+        <div className="col">
+          <SpendingForm/>
+        </div>
+      </div>
     </div>
   )
 }
