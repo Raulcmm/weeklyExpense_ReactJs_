0c575e8f41d46a63f55760266b2423ed0b088295
Raúl <osvaldoracm@gmail.com>
2020-07-22 20:17:49 -0500

    change of components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import BudgetForm from './components/BudgetForm'
+import React, { useState, Fragment } from 'react';
+import NewBudgetForm from './components/NewBudgetForm';
+import BudgetForm from './components/BudgetForm';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
-  return (
-    <div className="container mt-5 text-center">
-      <h1>Weekly expense</h1>
-
-      <div className="row">
-        <div className="col">
-          <BudgetForm/>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default App
+	const [ budget, setBudget ] = useState(0);
+	return (
+		<div className="container mt-5 text-center">
+			<h1>Weekly expense</h1>
+			{budget === 0 ? (
+				<BudgetForm setBudget={setBudget} />
+			) : (
+				<div className="card mt-5 row ">
+					<div className="row">
+						<div className="col-md-6">
+							<NewBudgetForm />
+						</div>
+						<div className="col-md-6">
+							<ExpenseList />
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+export default App;
