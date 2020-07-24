c6b40953a46c79c0ca965dc08bc7b3ce50561a02
Raúl <osvaldoracm@gmail.com>
2020-07-24 16:33:06 -0500

    way to show the components and amount left added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState} from 'react';
 import NewBudgetForm from './components/NewBudgetForm';
 import BudgetForm from './components/BudgetForm';
 import ExpenseList from './components/ExpenseList';
 
 const App = () => {
-	const [ budget, setBudget ] = useState(0);
+	const [ budgetTotal, setBudgetTotal ] = useState(0);
+	const [ budgetList, setBudgetList ] = useState([]);
+
 	return (
 		<div className="container mt-5 text-center">
 			<h1>Weekly expense</h1>
-			{budget === 0 ? (
-				<BudgetForm setBudget={setBudget} />
-			) : (
-				<div className="card mt-5 row ">
+			<div className=" card mt-4">
+				{budgetTotal  ? (
 					<div className="row">
 						<div className="col-md-6">
-							<NewBudgetForm />
+							<NewBudgetForm setBudgetList={setBudgetList} />
 						</div>
 						<div className="col-md-6">
-							<ExpenseList />
+							<ExpenseList budgetList={budgetList} budgetTotal={budgetTotal} />
 						</div>
 					</div>
-				</div>
-			)}
+				) : (
+					<BudgetForm setBudgetTotal={setBudgetTotal} />
+				)}
+			</div>
 		</div>
 	);
 };
